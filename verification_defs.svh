9739c119e6eac33705faf9b7f558bf90d6c9ab45
IkennaOkafor <ikennajeshuaokafor@gmail.com>
2022-03-24 10:28:12 -0400

    Updates

--- verification_defs.svh
+++ verification_defs.svh
@@ -3,7 +3,8 @@
 
 
 typedef enum bit {TRUE = 1, FALSE = 0} bool;
-typedef bit [127:0] mem_queue_64_t[$];
+typedef bit [127:0] mem_queue_128_t[$];
+typedef bit [63:0] mem_queue_64_t[$];
 
 
 function int max(int a, int b);
